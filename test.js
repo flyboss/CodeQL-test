cc51a5155ace677651240d69f0af93e239284e39
lw <915741005@qq.com>
2021-07-23 11:10:33 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,24 @@
-x == 1;
+let actuallyASource = wx.getUserInfo
+
+Page({
+    data: {
+        motto: "Hello World",
+        userInfo: actuallyASource(),
+    },
+    case1: function() {
+        let info = this.data.userInfo;
+        let sinkInDisguise = wx.request;
+        sinkInDisguise(info);
+    },
+    case2: function() {
+        const info = wx.getUserInfo();
+        wx.request(info);
+    },
+    case3: function() {
+        const info = wx.getUserInfo();
+        wx.request({
+            url: localhost,
+            data: info
+        })
+    }
+});
