14dfe67d2aa6caca085a0679e17ffdc83a0a678b
lw <915741005@qq.com>
2021-07-23 11:54:01 +0800

    Update test.js

--- test.js
+++ test.js
@@ -22,3 +22,17 @@ Page({
         })
     }
 });
+
+const fs = require('fs'),
+    path = require('path');
+
+function readFileHelper(p) { // #2
+    p = path.resolve(p); // #3
+    fs.readFile(p, // #4
+        'utf8', (err, data) => {
+            if (err) throw err;
+            console.log(data);
+        });
+}
+
+readFileHelper(process.argv[2]); // #1
