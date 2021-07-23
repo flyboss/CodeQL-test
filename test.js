67bdf265a73e807a7450f03739d881f1953e416d
lw <915741005@qq.com>
2021-07-23 18:42:54 +0800

    Update test.js

--- test.js
+++ test.js
@@ -27,7 +27,7 @@ const fs = require('fs'),
     path = require('path');
 
 function readFileHelper(p) { // #2
-    p = path.resolve(p); // #3
+    // p = path.resolve(p); // #3
     fs.readFile(p, // #4
         'utf8', (err, data) => {
             if (err) throw err;
