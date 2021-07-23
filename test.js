ad832edc5c4a746830139e7d6750da29b1c17ca8
lw <915741005@qq.com>
2021-07-23 12:01:39 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,27 +1,27 @@
-// let actuallyASource = wx.getUserInfo
+let actuallyASource = wx.getUserInfo
 
-// Page({
-//     data: {
-//         motto: "Hello World",
-//         userInfo: actuallyASource(),
-//     },
-//     case1: function() {
-//         let info = this.data.userInfo;
-//         let sinkInDisguise = wx.request;
-//         sinkInDisguise(info);
-//     },
-//     case2: function() {
-//         const info = wx.getUserInfo();
-//         wx.request(info);
-//     },
-//     case3: function() {
-//         const info = wx.getUserInfo();
-//         wx.request({
-//             url: localhost,
-//             data: info
-//         })
-//     }
-// });
+Page({
+    data: {
+        motto: "Hello World",
+        userInfo: actuallyASource(),
+    },
+    case1: function() {
+        let info = this.data.userInfo;
+        let sinkInDisguise = wx.request;
+        sinkInDisguise(info);
+    },
+    case2: function() {
+        const info = wx.getUserInfo();
+        wx.request(info);
+    },
+    case3: function() {
+        const info = wx.getUserInfo();
+        wx.request({
+            url: localhost,
+            data: info
+        })
+    }
+});
 
 const fs = require('fs'),
     path = require('path');
